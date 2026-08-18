4c9eb9951086ba46cd21fb511012feebe7e1487f
amandanayumi <amandanayumi@hotmail.com>
2026-08-18 19:21:18 -0300

    Clean up comments in main.py
    Removed comments from the main function.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 def main():
-    # Solicita os números ao usuário
+    
     numero1 = float(input("Digite o primeiro número: "))
     numero2 = float(input("Digite o segundo número: "))
     
-    # Calcula a soma
     soma = numero1 + numero2
     
-    # Exibe o resultado
     print(f"\n A soma de {numero1} + {numero2} = {soma}")
 
 if __name__ == "__main__":
