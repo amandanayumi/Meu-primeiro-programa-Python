c607bfe824228f36a71b8fb700d0b71c0545bec8
amandanayumi <amandanayumi@hotmail.com>
2026-09-08 20:42:01 -0300

    Implement grade sum calculation in main function
    Added a main function to calculate the sum of four grades.

--- 080926main.py
+++ 080926main.py
@@ -1,9 +1,14 @@
+def main():
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    nota3 = float(input("Digite a terceira nota: "))
+    nota4 = float(input("Digite a quarta nota:" ))
 
-print ("Hello World")
-#
+    soma = nota1 + nota2 + nota3 + nota4
 
+    print("Resultado da soma das notas:", soma)
 
-
+main()
 
 
 
